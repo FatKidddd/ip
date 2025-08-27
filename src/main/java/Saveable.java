678f17e3e77c792cb0d3225e227bf487190b352c
Justin Wong <justinwongjianhui@gmail.com>
2025-08-27 22:27:07 +0800

    Add Storage class and Saveable interface

---
+++ src/main/java/Saveable.java
@@ -0,0 +1,20 @@
+public interface Saveable {
+    String toSaveFormat();
+    
+    static Task fromSaveFormat(String line) throws TinManException {
+        String[] parts = line.split(" \\| ");
+        if (parts.length < 3) {
+            throw new TinManException("Invalid task format in data file");
+        }
+        
+        String typeCode = parts[0];
+        boolean isDone = "1".equals(parts[1]);
+        
+        CommandType commandType = CommandType.fromSaveTypeCode(typeCode);
+        if (commandType == CommandType.UNKNOWN) {
+            throw new TinManException("Unknown task type in data file: " + typeCode);
+        }
+        
+        return commandType.createFromSaveFormat(parts, isDone);
+    }
+}
